a36eb9d4f37df63b596e8e6982d1c87f16f29c5c
JRaphaelO <jraphaeloliveira14@gmail.com>
2021-01-26 20:51:07 -0300

    fix error eslint, in directory src

--- mobile/icodsMobile/src/App.tsx
+++ mobile/icodsMobile/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-const App: React.FC = () => {
+const App = () => {
   <View>
     <Text>ICods 🦾</Text>
   </View>
 }
+
+export default App

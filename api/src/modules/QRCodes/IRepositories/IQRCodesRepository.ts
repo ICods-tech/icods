b6c890798c745cda0a803194a27e03fe5b60dfa7
Lucas Moreira <lucasmsea@outlook.com>
2021-05-31 12:28:34 -0300

    feat: add retrive deactivated qr codes route and its related service

--- api/src/modules/QRCodes/IRepositories/IQRCodesRepository.ts
+++ api/src/modules/QRCodes/IRepositories/IQRCodesRepository.ts
@@ -3,12 +3,13 @@ import User from '@modules/Users/infra/typeorm/models/user';
 import { Colors } from '../interfaces/Colors';
 
 export default interface IQRCodesRepository {
-  get(id: string): Promise<QRCode | undefined>;
-  activate(id: string, user: Omit<User, 'created_at' | 'updated_at' | 'password' | 'qrcodes'>): Promise<QRCode>;
   create(): Promise<QRCode>;
   delete(id: string): Promise<void>;
-  changeFavoriteStatus(qrCode: QRCode): Promise<QRCode>;
-  changeQRCodeColor(qrCode: QRCode, color: Colors, type: 'madeColor' | 'receivedColor'): Promise<QRCode>;
-  receiveQRCode(qrCode: QRCode, user: Omit<User, 'created_at' | 'updated_at' | 'password' | 'qrcodes'>): Promise<QRCode>;
   save(qrCode: QRCode): Promise<void>;
+  get(id: string): Promise<QRCode | undefined>;
+  changeFavoriteStatus(qrCode: QRCode): Promise<QRCode>;
+  getMultipleDeactivatedQRCodes(numberOfQrCodes: number): Promise<QRCode[] | []>;
+  changeQRCodeColor(qrCode: QRCode, color: Colors, type: 'madeColor' | 'receivedColor'): Promise<QRCode>;
+  activate(id: string, user: Omit<User, 'created_at' | 'updated_at' | 'password' | 'qrcodes'>): Promise<QRCode>;
+  receiveQRCode(qrCode: QRCode, user: Omit<User, 'created_at' | 'updated_at' | 'password' | 'qrcodes'>): Promise<QRCode>;
 }
--- api/src/modules/QRCodes/infra/http/controller/DeactivatedQRCodeController.ts
+++ api/src/modules/QRCodes/infra/http/controller/DeactivatedQRCodeController.ts
@@ -1,8 +1,23 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 import CreateDeactivatedQRCodeService from '@modules/QRCodes/services/CreateDeactivatedQRCodeService'
+import GetDeactivatedQRCodeService from '@modules/QRCodes/services/GetDeactivatedQRCodesService'
 
-export default class CreateQRCodeController {
+export default class DeactivatedQRCodeController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    try {
+      const numberOfQrCodes = parseInt(request.query.numberOfQrCodes as string) || 1
+      const getDeactivatedQRCode = container.resolve(GetDeactivatedQRCodeService)
+
+      const qrCodes = await getDeactivatedQRCode.run(numberOfQrCodes)
+
+      return response.json(qrCodes)
+    } catch (err) {
+      console.log(err)
+      return response.status(400).json(err.message)
+    }
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     try {
       const numberOfQrCodes = request.body?.numberOfQrCodes || null

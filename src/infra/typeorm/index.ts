ceb2bdad4993d579e612014d2dbcb231fcebcae8
Lucas <lucasmsea@outlook.com>
2021-10-29 16:28:46 -0300

    feat: add initial mail sending functionality to user signUp

--- src/infra/typeorm/index.ts
+++ src/infra/typeorm/index.ts
@@ -4,5 +4,7 @@ import { createConnection } from "typeorm";
 export async function startConnection(): Promise<any> { await run() }
 
 async function run() {
+  console.log("Starting connection...")
   const connection = await createConnection()
+  console.log(connection.isConnected ? "Connection established" : "Connection failed")
 }

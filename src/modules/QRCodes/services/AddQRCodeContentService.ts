9baac21db6ce155f73a724cf0b273a3f6be7346c
Thomas <thomasaraujo@mat.ci.ufpb.br>
2021-11-18 15:18:23 -0300

    feat: assign user to QRCode

--- src/modules/QRCodes/services/AddQRCodeContentService.ts
+++ src/modules/QRCodes/services/AddQRCodeContentService.ts
@@ -2,26 +2,32 @@ import QRCode from '@modules/QRCodes/typeorm/models/QRCode';
 import IQRCodesRepository from '@modules/QRCodes/interfaces/IQRCodesRepository';
 import AppError from '../../../infra/error/AppError'
 import { inject, injectable } from 'tsyringe'
+import IUserRepository from '@modules/Users/interfaces/IUserRepository';
 
 @injectable()
 export default class AddQRCodeToUserService {
 
   constructor(
     @inject('QRCodeRepository')
-    private qrCodesRepository: IQRCodesRepository
+    private qrCodesRepository: IQRCodesRepository,
+    @inject('UsersRepository')
+    private userRepository: IUserRepository
   ) { }
 
   public async run( qrcode_id: string,
     name: string,
     size: number,
     key: string,
-    url: string): Promise<QRCode> {
+    url: string,
+    userId: string): Promise<QRCode> {
     try {
       const qrcode = await this.qrCodesRepository.get(qrcode_id) as QRCode
+      const user = await this.userRepository.findById(userId);
 
       qrcode.link = url;
       qrcode.status = 'IN_PROGRESS';
       qrcode.content = name;
+      qrcode.user = user;
       await this.qrCodesRepository.save(qrcode)
 
       return qrcode

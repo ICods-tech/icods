256e8862267dbde525a24f341c39fae59f0e1794
Lucas <lucasmsea@outlook.com>
2021-11-19 10:07:57 -0300

    refactor: adjust service naming on DeleteUserController

--- src/modules/Users/controller/DeleteUserController.ts
+++ src/modules/Users/controller/DeleteUserController.ts
@@ -1,13 +1,12 @@
-import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express'
-import SignInService from '@modules/Users/services/user/DeleteUserService'
+import DeleteUserService from '@modules/Users/services/user/DeleteUserService'
 import { container } from 'tsyringe'
 
 export default class DeleteUserController {
   public async delete(request: Request, response: Response): Promise<Response> {
     try {
       const { id } = request.user
-      const deleteUserService = container.resolve(SignInService)
+      const deleteUserService = container.resolve(DeleteUserService)
 
       await deleteUserService.run(id)
 
